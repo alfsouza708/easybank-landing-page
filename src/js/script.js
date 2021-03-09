262175d611791762fac47443f2d134605633d184
Lucas Souza <alfsouza708@gmail.com>
2021-03-09 15:43:06 -0300

    adiciona animação hamburger menu e overlay

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const btnHamburger = document.querySelector("#btnHamburger")
+
+btnHamburger.addEventListener("click", () => {
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open")
+  } else {
+    btnHamburger.classList.add("open")
+  }
+})
