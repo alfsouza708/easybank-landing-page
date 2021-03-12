d28cbe92e3145000298bbea4fabb13a9250f3dc6
Lucas Souza <alfsouza708@gmail.com>
2021-03-11 22:09:06 -0300

    adiciona menu overlay

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,23 @@
 const btnHamburger = document.querySelector("#btnHamburger")
+const body = document.querySelector("body")
 const header = document.querySelector("header")
 const overlay = document.querySelector(".overlay")
+const fadeElems = document.querySelectorAll(".has-fade")
 
 btnHamburger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
     header.classList.remove("open")
-    overlay.classList.remove("fade-in")
-    overlay.classList.add("fade-out")
+    body.classList.remove("no-scroll")
+    fadeElems.forEach((el) => {
+      el.classList.remove("fade-in")
+      el.classList.add("fade-out")
+    })
   } else {
     header.classList.add("open")
-    overlay.classList.remove("fade-out")
-    overlay.classList.add("fade-in")
+    body.classList.add("no-scroll")
+    fadeElems.forEach((el) => {
+      el.classList.remove("fade-out")
+      el.classList.add("fade-in")
+    })
   }
 })
